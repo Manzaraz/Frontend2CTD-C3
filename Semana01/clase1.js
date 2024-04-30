d0c8d65d3b4abb31b122114755b09c294ca980f5
Christian Manzaraz <magnusmanz@gmail.com>
2024-04-29 23:57:56 -0300

    Clase 01: Fin del juego

--- Semana01/clase1.js
+++ Semana01/clase1.js
@@ -3,9 +3,28 @@
 /* -------------------------------------------------------------------------- */
 
 function iniciarJuego() {
- 
+    // Saludando al usuario
+    alert("Bienvenido al juego Piedra🗿, Papel 📃 o Tijera✂️ de Frontend II")
+
+    // Pedir el nombre al usuario y guardarlo en una variable
+    let nombre = prompt("Ingrese su nombre por favor.")
+
+    //    alert("Gracias por jugar " + nombre + ", ¡Mucha suerte!🍀")
+    alert(`Gracias por jugar ${nombre}, ¡Mucha suerte!🍀`)
+
+    // Mostramos datos por consola
+    console.log("--------------")
+    console.log("El jugador es:")
+    console.log(nombre)
+    console.log("--------------")
+    
+
+    return nombre
 }
 
+const nombreJugador = iniciarJuego()
+console.log(nombreJugador.length);
+console.log(isNaN(nombreJugador));
 
 /* -------------------------------------------------------------------------- */
 /*                          CONSIGNA MESA DE TRABAJO                          */
@@ -13,6 +32,7 @@ function iniciarJuego() {
 // 1- Modificar la funcion de iniciarJuego(), validar si ingresa un dato válido como nombre.
 // 2- Si no ingresa un texto, o tiene menos de 3 caracteres debemos volverle a pedir que lo ingrese.
 // 3- Finalmente el nombre devuelto debe estar todo en mayúsculas.
+
 
 
 

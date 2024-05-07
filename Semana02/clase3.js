4d26a82f9f1757148668adaffb58b37d6aecb500
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-07 00:26:47 -0300

    Clase 03: Inicio del juego

--- Semana02/clase3.js
+++ Semana02/clase3.js
@@ -1,4 +1,5 @@
 // 👇Acá vemos que el document nos dá acceso a todo el DOM, ese arbol que contiene todos los nodos de nuestro sitio
+// console.log(document);
 
 
 /*
@@ -8,46 +9,95 @@ Ahora vamos a utilizar 2 métodos propios de document que nos facilitan "pescar"
 */
 
 // Obtenemos el titulo principal
-
-
+const titulo = document.querySelector("h1")
 // Ahora vayamos a la consola y despleguemos la flecha que nos muestra todas las propiedades del nodo
+// console.log(titulo);
+// console.log(titulo.textContent);
+// console.log(titulo.innerText);
 
 
 // nos traemos ahora un listado de nodos 👇
-
-
+const itemsMenu = document.querySelector(".info")
+const infoExtra = document.querySelector(".info .clima")
+const navItems = document.querySelectorAll("ul li")
 // hacemos un selector más específico👇
+const imagen = document.querySelector("[src='./img/futbol.jpeg']")
+
+
+// console.log(itemsMenu);
+// console.log(infoExtra);
+// console.log(navItems);
+// console.log(imagen);
+// console.log(itemsMenu.children);
 
 /* ------------------------------- Practicando ------------------------------ */
 // Seleccionamos la lista de noticias y revisamos su informacion interna.
 // Aprovechamos que la NodeList es un ITERABLE, entonces podemos recorrerla.
 // 🚩 No es un Array.
-
+const articulos = document.querySelectorAll("article")
+// console.log(articulos);
 
 // Ahora hacemos la misma practica pero con ForEach, y accedemos a propiedades de los nodos.
-
+// for (let i = 0; i < articulos.length; i++) {
+//     console.log(articulos[i])
+// }
 
 // vamos a practiacar con ForEach
-
+articulos.forEach(function (articulo) {
+    const titular = articulo.querySelector("h2").textContent
+    // console.log(titular)
+})
 
 // for (const noticia of articulos) {
-
+for (const noticia of articulos) {
+    const texto = noticia.querySelector("p").innerText
+    // console.log(texto);
+    // console.log(texto.length);
+}
 
 // 💪🏼 Ahora llevemos esto un paso adelante!
 // Vamos a interactuar con el DOM para agregarle mas estilos a nuestro sitio.
 // 👇 Primero capturemos todos los elementos que vamos a modificar.
+const sitio = document.querySelector("body")
+const btnTema = document.querySelector(".tema button")
+const menuItems = document.querySelectorAll("nav li")
+const contenedorDeNoticias = document.querySelector("main")
 
 
-
+// console.log(contenedorDeNoticias);
 
 /* ---------------------------- Editado los ITEMS --------------------------- */
 // 👇 Acá podemos ver todas las propiedades CSS que podemos modificar con JS
+// console.log(menuItems[2].style)
 
+menuItems.forEach( (item) => {
+    item.style.textTransform = "uppercase"
+    item.style.color = "aqua"
+    item.style.backgroundColor = "rgba(255, 255, 255, 0.2)"
+    item.style.borderRadius = "50vh"
+    item.style.padding = "10px"
+    item.style.marginTop = "20pox"
+})
 
 
 /* ----------------------------- Editando clases ---------------------------- */
-
 // vamos probando uno a uno los métodos
+console.log(sitio.classList)
+console.log(sitio.classList.contains("dark"))
+
+console.log(sitio.classList)
+console.log(sitio.classList.add("dark"))
+
+console.log(sitio.classList)
+console.log(sitio.classList.remove("dark"))
+
+console.log(sitio.classList)
+console.log(sitio.classList.toggle("dark"))
+console.log(sitio.classList.toggle("dark"))
+console.log(sitio.classList.toggle("dark"))
+
+
+
 
 /* -------------------------------------------------------------------------- */
 /*                          CONSIGNA MESA DE TRABAJO                          */

dd3354767278abf6f2a5ad11f02c324b9e98b527
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-22 21:53:05 -0300

    Clase 08: Inicio de clase

---
+++ Semana04/clase8.js
@@ -0,0 +1,89 @@
+/* -------------------------------------------------------------------------- */
+/*               [1] FUNCION: capturar los datos del formulario               */
+/* -------------------------------------------------------------------------- */
+function capturarDatosFormulario() {
+    // 👇🏼 Establecer un objeto vacio para despues rellenarlo con los datos del form, 
+    const objetoInformacion = {
+        nombre: "",
+        password: "",
+        telefono: "",
+        hobbies: [],
+        nacionalidad: "",
+    }
+
+    // Capturamos los nodos de nuestro DOM, inputs
+    
+
+    // Fieldset Hobbies
+    
+    
+    // Fieldset Nacionalidad
+    
+
+
+    // Empezamos a completar el objetoInformacion con la info pertiente
+    
+}
+capturarDatosFormulario()
+
+/* -------------------------------------------------------------------------- */
+/*                 [2] FUNCION: escuchamos el submit del form                 */
+/* -------------------------------------------------------------------------- */
+const form = document.querySelector("form")
+
+form.addEventListener("submit", function (ev) {
+    // Prevenimos el comportamiento por defecto del HTML
+    
+
+    //pedir los datos ingresados en los inputs invocando a la funcion de arriba
+    
+    // con los errores capturados necesito pintarlos en pantalla para darle un feedback al usuario
+    // muestro un cuadro de dialogo con los errores para que los corrija
+
+
+    // muestro cuadro de dialogo indicando que todo esta bien
+
+})
+
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                       [3] FUNCION: renderizar errores                      */
+/* -------------------------------------------------------------------------- */
+// Desarrollamos esta funcion para llamarla en el submit
+function renderizarErrores(listado) {
+
+}
+
+
+/* -------------------------------------------------------------------------- */
+/*                         [4] FUNCION: validar datos                         */
+/* -------------------------------------------------------------------------- */
+// Desarrollar la funcion 3 de validar los datos.
+// Esta funcion va a recibir un objeto con la misma estructura de objetoInformacion
+// Entonces dentro de esta función vamos a chequear ciertas validaciones.
+// 1- La funcion devuelve un listado de errores según las comprobaciones que hace sobre el objeto.
+// 2- Si el nombre no es un texto y tiene menos de 3 caracteres sumar el error: "El nombre debe tener al menos 3 caracteres."
+// 3- Si la contraseña tiene menos de 6 caracteres, sin contar espacios al principio, en el medio o final, sumar el error: "La contraseña debe tener al menos 6 caracteres, entre letras y símbolos."
+// 4- Si el telefono tiene menos de 10 números, sumar el error: "No es un teléfono válido."
+// 5- Si la lista de hobbies tiene más de 4 items, sumar el error: "Sólo es posible seleccionar 4 hobbies."
+// 5- Si no hay una nacionalidad definida, sumar el error: "Debe seleccionar una nacionalidad."
+function validarInformacion(usuario) {
+
+}
+
+/* -------------------------------------------------------------------------- */
+/*     Mesa de Trabajo - [5] FUNCION: Formulario completado con éxito         */
+/* -------------------------------------------------------------------------- */
+// Esta funcion se va a encargar de mostrar que el formulario se completó correctamente.
+// Para eso debera cumplir con los siguientes requerimientos.
+// 1 - Recibe el listado de errores, y solo si no hay ninguno debe:
+// 2 - mostrar al final del formulario un caja con la misma estructura que la caja de errores, pero con la tonalidad verde
+// 3 - dentro la caja debe mostrar un párrafo con el mensaje: "¡Formulario completado con éxito!"
+// 4 - a su vez se debe deshabilitar el boton del formulario
+// 5 - finalmente pasados 4 segundos: se debe eliminar esa caja, habilitar el boton y limpiar el formulario
+
+function mostrarMensajeExito(listado) {
+
+}
